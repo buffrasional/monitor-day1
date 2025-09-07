c9fce378253bfcbc0a248a18daf826d4ec3d84d3
buffrasional <74383698+buffrasional@users.noreply.github.com>
2025-09-07 17:55:18 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Preview from "./Preview";
+import Preview from "./preview";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
