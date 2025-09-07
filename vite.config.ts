6c5ad8aac919f41da7d971f8522989b2158387af
buffrasional <74383698+buffrasional@users.noreply.github.com>
2025-09-07 17:50:29 +0700

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// @ts-ignore - plugin-react will be used if installed by vite's react template
-export default defineConfig({
-  plugins: [react()],
-});
